ce26ec6ccdfeb4eb20286fdb251cdcdfd8c613b2
Chris Otto <chris.otto6@gmail.com>
2019-08-21 21:46:56 -0500

    Changing the site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gastify`,
+    description: `Infinite scroll image gallery built with Gatsby and Netlify`,
+    author: `@wisco_cmo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
